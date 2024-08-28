400fc1fabe769220fcc20f52e3f26bd7271c0a75
HitOnMe <levietminh93@gmail.com>
2024-08-28 22:30:06 +0700

    fix link

--- service/fetch.js
+++ service/fetch.js
@@ -1,8 +1,8 @@
 export let getData = async() => {
     try {
-        const response = await fetch('../data/Data.json');
+        const response = await fetch('https://66adc881b18f3614e3b5e0b7.mockapi.io/product');
         const data = await response.json();
-        return data
+        return data[0]
     } catch(error){
         console.error(error)
     }
