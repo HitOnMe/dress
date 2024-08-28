5e7498dadaba05284b7d84c4aa7c8586a8937f64
HitOnMe <levietminh93@gmail.com>
2024-08-28 22:57:47 +0700

    clean code

--- models/index.js
+++ models/index.js
@@ -1,48 +1,24 @@
-import {domID, select, selectID, selectAll} from './DOM.js';
-import {getData} from '../service/fetch.js';
-import setArray, {navPills, tabPanes, showPart, Character} from './method.js';
-document.addEventListener('DOMContentLoaded', (e) => {
-  e.preventDefault;
-  const triggerTabList = document.querySelectorAll("#showNav li");
+import { domID, selectAll } from './DOM.js';
+import { getData } from '../service/fetch.js';
+import setArray, { NavPills, TabPanes, ShowPart, Character } from './method.js';
 
-  triggerTabList.forEach((triggerEl) => {
-    const tabTrigger = new bootstrap.Tab(triggerEl);
-  
-    triggerEl.addEventListener("click", (event) => {
-      event.preventDefault();
-      tabTrigger.show();
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const objects = await getData();
+    const navbar = new NavPills(objects.navPills);
+    const tab = new TabPanes(objects.navPills);
+
+    domID('showNav').innerHTML = navbar.showNav();
+    domID('showTab').innerHTML = tab.renderTab();
+
+    setArray.forEach((attribute) => {
+      const part = new ShowPart(objects.tabPanes, attribute);
+      domID(attribute).innerHTML = part.renderTab();
     });
-  });
 
-  getData().then(objects => {
-      const navbar = new navPills(objects.navPills);
-      const tab = new tabPanes(objects.navPills)
-      
-     
-      /* console.log(setArray)
-      console.log(objects.navPills) */
-      domID('showNav').innerHTML += navbar.showNav();
-      domID('showTab').innerHTML += tab.renderTab();
-      console.log(setArray)
-      setArray.forEach((atribute, index) => {
-          let part = new showPart(objects.tabPanes, atribute)
-          domID(atribute).innerHTML = part.renderTab();
-          
-          /* new Character(setArray).show(selectID('contain')[index], selectAll('.showButton')) */
-      });
-      
-      new Character(setArray).show(selectAll('.showButton'), selectAll('.contain div'))
-      
-      /* select() */
-
-  })
-  
-})
-/* selectAll('.showButton').forEach(button => {
-  button.onclick = function(){
-    let id = this.parentNode.parentNode.parentNode.id;
-    let 
-    let myCharacter = new Character()
-
+    const character = new Character(setArray);
+    character.show(selectAll('.showButton'), selectAll('.contain div'));
+  } catch (error) {
+    console.error("Error loading data:", error);
   }
-}) */
+});
--- models/method.js
+++ models/method.js
@@ -1,54 +1,53 @@
-
 let setArray = [];
 let partObject = {
   'topclothes': 'bikinitop',
-  'botclothes' : 'bikinibottom',
-  'shoes' : 'feet',
+  'botclothes': 'bikinibottom',
+  'shoes': 'feet',
   'handbags': 'handbag',
-  'necklaces' : 'necklace',
-  'hairstyle' : 'hairstyle',
-  'background' : 'background'
-}
+  'necklaces': 'necklace',
+  'hairstyle': 'hairstyle',
+  'background': 'background'
+};
+
 export default setArray;
-class setPart {
-  constructor(id, type, name, desc, imgSrc_jpg, imgSrc_png){
-    this.id = id,
-    this.type = type,
-    this.name = name,
-    this.desc = desc,
-    this.imgSrc_jpg = imgSrc_jpg,
-    this.imgSrc_png = imgSrc_png
+
+class SetPart {
+  constructor(id, type, name, desc, imgSrc_jpg, imgSrc_png) {
+    this.id = id;
+    this.type = type;
+    this.name = name;
+    this.desc = desc;
+    this.imgSrc_jpg = imgSrc_jpg;
+    this.imgSrc_png = imgSrc_png;
   }
-  render(){
+
+  render() {
     let myID = this.id.replace(/_/g, "");
-    let myType = this.type == 'topclothes' |  this.type == 'botclothes' ?  'clothes' : this.type;
-    return ` <img
-                src="./assets/images/${myType}/${myID}_show.jpg"
-                class="card-img-top"
-                alt=${this.desc}
-                type = ${this.type}
-                    />`
+    let myType = (this.type === 'topclothes' || this.type === 'botclothes') ? 'clothes' : this.type;
+    return `
+      <img src="../assets/images/${myType}/${myID}_show.jpg"
+           class="card-img-top"
+           alt="${this.desc}"
+           type="${this.type}" />`;
   }
 }
 
-class tabPane extends setPart {
-  constructor(id, type, name, desc, imgSrc_jpg, imgSrc_png){
-    super(id, type, name, desc, imgSrc_jpg, imgSrc_png);
-  };
-  showSet(){
-    
-    return `<div class="card col-3" id = ${this.id} type=${partObject[this.type]}>
-                    ${this.render()}
-                    <div class="card-body">
-                      <h5 class="card-title text-center">${this.name}</h5>
-                      <div class="row">
-                        <button href="#" class="col-12 showButton">Thử đồ</button>
-                      </div>
-                    </div>
-                  </div>`
+class TabPane extends SetPart {
+  showSet() {
+    return `
+      <div class="card col-3" id="${this.id}" type="${partObject[this.type]}">
+        ${this.render()}
+        <div class="card-body">
+          <h5 class="card-title text-center">${this.name}</h5>
+          <div class="row">
+            <button class="col-12 showButton">Thử đồ</button>
+          </div>
+        </div>
+      </div>`;
   }
 }
-class navPill {
+
+class NavPill {
   constructor(tabName, showName, type) {
     this.tabName = tabName;
     this.showName = showName;
@@ -60,108 +59,95 @@ class navPill {
     const isActive = this.type === 'topclothes' ? 'active' : '';
     return `
       <li class="nav-item col" role="presentation">
-        <button class="nav-link ${isActive}" id="home-tab" data-toggle="tab" data-target="#${this.type}" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">
+        <button class="nav-link ${isActive}" id="home-tab" data-toggle="tab" 
+          data-target="#${this.type}" type="button" role="tab">
           ${this.showName}
         </button>
       </li>`;
   }
 }
 
-
-export class navPills {
-    constructor(objects){
-      this.objects = objects
-    }
-    showNav(){
-      let contentHTML = '';
-      this.objects.forEach(object =>{
-        let navbar = new navPill(object.tabName, object.showName, object.type);
-        contentHTML+=navbar.render()
-      })
-      
-      return `${contentHTML}`
-    }
-}
-
-
-
-export class tabPanes{
-  constructor(objects){
+export class NavPills {
+  constructor(objects) {
     this.objects = objects;
   }
-  renderTab(){
-    let contentHTML = '';
-    this.objects.forEach(object =>{
-      
-        const thisShow  = object.type == 'topclothes' ? 'active show' : '';
-      let content = `
-              <div
-                class="tab-pane row fade ${thisShow}"
-                id = ${object.type}
-                role="tabpanel"
-                aria-labelledby="nav-home-tab"
-                tabindex="0"
-              >
-                </div>`;
-          contentHTML += content;
-    });
-  
-    
-    return `<div class="tab-content" id="nav-tabContent">
-              ${contentHTML}</div>`
+
+  showNav() {
+    return this.objects.map(object => {
+      const navbar = new NavPill(object.tabName, object.showName, object.type);
+      return navbar.render();
+    }).join('');
   }
 }
 
-export class showPart{
-  constructor(objects, part){
-    this.part = part
-    this.objects = objects.filter(object => object.type == this.part)
+export class TabPanes {
+  constructor(objects) {
+    this.objects = objects;
   }
-  renderTab(){
-    let contentHTML = '';
-    this.objects.forEach(object =>{
-      let tabBar = new tabPane(object.id, object.type, object.name, object.desc, object.imgSrc_jpg);
-      contentHTML+=tabBar.showSet()
-    })
-    
-    return contentHTML
+
+  renderTab() {
+    return `
+      <div class="tab-content" id="nav-tabContent">
+        ${this.objects.map(object => {
+          const thisShow = object.type === 'topclothes' ? 'active show' : '';
+          return `
+            <div class="tab-pane row fade ${thisShow}" id="${object.type}" 
+              role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0"></div>`;
+        }).join('')}
+      </div>`;
   }
 }
 
-class getPart {
-  constructor(type, button){
+export class ShowPart {
+  constructor(objects, part) {
+    this.part = part;
+    this.objects = objects.filter(object => object.type === this.part);
+  }
+
+  renderTab() {
+    return this.objects.map(object => {
+      const tabBar = new TabPane(object.id, object.type, object.name, object.desc, object.imgSrc_jpg);
+      return tabBar.showSet();
+    }).join('');
+  }
+}
+
+class GetPart {
+  constructor(type, button) {
     this.type = type;
-    this.button = button
+    this.button = button;
   }
-  getClothes(){
-      let showDiv = this.button.parentNode.parentNode.parentNode;
-      let myID = showDiv.id.replace(/_/, '');
-      let myType = this.type == 'topclothes' ||  this.type == 'botclothes' ?  'clothes' : this.type;
-      let file = myType == 'background' ? 'jpg' : 'png'
-      return `url('./assets/images/${myType}/${myID}.${file}')`
+
+  getClothes() {
+    const showDiv = this.button.closest('.card');
+    const myID = showDiv.id.replace(/_/, '');
+    const myType = (this.type === 'topclothes' || this.type === 'botclothes') ? 'clothes' : this.type;
+    const file = myType === 'background' ? 'jpg' : 'png';
+    return `url('./assets/images/${myType}/${myID}.${file}')`;
   }
 }
+
 export class Character {
-  constructor(...clothes){
-    this.hairstyle=clothes[0],
-    this.necklace = clothes[1],
-    this.bikinitop = clothes[2],
-    this.bikinibottom = clothes[3],
-    this.handbag = clothes[4],
-    this.feet = clothes[5],
-    this.background = clothes[6];
+  constructor(...clothes) {
+    [
+      this.hairstyle,
+      this.necklace,
+      this.bikinitop,
+      this.bikinibottom,
+      this.handbag,
+      this.feet,
+      this.background
+    ] = clothes;
   }
-  show(buttons, ids){
+
+  show(buttons, ids) {
     buttons.forEach(button => {
       button.addEventListener('click', () => {
-      let myType = button.parentNode.parentNode.parentNode.getAttribute('type')
-      let choosePart = Array.from(ids).filter(id => id.classList[0] == myType)[0];
-      choosePart.style.background = new getPart(Object.keys(partObject).find(key => partObject[key] == myType), button).getClothes();
-      choosePart.style.backgroundSize = 'cover';
-      }
-      )
-    })
-  
+        const myType = button.closest('.card').getAttribute('type');
+        const choosePart = Array.from(ids).find(id => id.classList.contains(myType));
+        choosePart.style.background = new GetPart(Object.keys(partObject).find(key => partObject[key] === myType), button).getClothes();
+        choosePart.style.backgroundSize = 'cover';
+      });
+    });
   }
- 
 }
